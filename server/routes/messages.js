783213312039b49aa9072e7995f1bf068538f66e
rikiparata <rikiipee47@gmail.com>
2018-07-22 12:54:03 +1200

    added a space for heroku push

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -3,12 +3,11 @@ const router = express.Router()
 
 const db = require('../db/messages')
 
-router.get('/:id', (req, res) => { // this route grabs posts -- with user information attached
-    const id = req.params.id // CHECK: best to attach id to route params?
+router.get('/:id', (req, res) => {
+    const id = req.params.id
 
     db.getMessagesByReceiver(id)
         .then(messages => {
-            console.log(messages)
             res.json(messages)
         })
         .catch(err => {
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -65,6 +65,6 @@ router.delete('/post/:id', (req, res) => {
       if (err) throw err
     })
 })
-
+ 
 
 module.exports = router
