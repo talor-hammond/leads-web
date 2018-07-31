55e817622355604258a449e5e8f3d4b0fdceddd7
talor-hammond <talorjhammond@gmail.com>
2018-07-31 19:55:25 +1200

    getPostByPostId gets a post obj by an id

--- test/server/db/db.test.js
+++ test/server/db/db.test.js
@@ -16,10 +16,16 @@ afterEach(() => { // destroying the connection; clean-slate
 
 // TESTS
 test('getPosts returns the correct array of data; length', () => {
-
     return posts.getPosts(testDb)
         .then(postsArray => {
             expect(postsArray).toHaveLength(1)
         })
+})
 
+test('getPostByPostId gets a post obj by an id', () => {
+    return posts.getPostByPostId(1, testDb)
+        .then(post => {
+            expect(post).toBeTruthy()
+            expect(post.post_id).toBe(1)
+        })
 })
