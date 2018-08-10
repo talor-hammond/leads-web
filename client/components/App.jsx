f09f6a99d06781496a8f6198a6efd1fb9312a24c
talor-hammond <talorjhammond@gmail.com>
2018-08-10 16:29:51 +1200

    added Footer component to all routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
+// Components:
 import Login from './Login'
 import Register from './Register'
 import HomeNav from './HomeNav'
 import Home from './Home'
+import Footer from './Footer'
 
 const App = () => (
   <Router>
@@ -13,6 +15,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/Register" component={Register} />
+      <Route path="/" component={Footer} />
     </div>
   </Router>
 )
