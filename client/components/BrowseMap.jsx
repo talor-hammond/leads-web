01314ef4e2fa857d4606da142662ecace3a5e10e
talor-hammond <talorjhammond@gmail.com>
2018-08-12 16:06:22 +1200

    init Map w api key

--- client/components/BrowseMap.jsx
+++ client/components/BrowseMap.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 // Components:
 // import Map from './Map'
@@ -14,7 +17,20 @@ class BrowseMap extends Component {
             <section className="section">
                 <div className="container">
                     {/* parse 'area' from google maps api w browsers' lat & long */}
-                    <h1 className="title">Leads in your area...</h1> 
+                    <h1 className="title">Leads in your area...</h1>
+                        <div className="map-container">
+                            <Map google={window.google}
+                                style={{ height: '80%', width: '100%', margin: 'none', left: 0, position: 'absolute' }}
+                                zoom={17}
+                                initialCenter={{
+                                    lat: -41.300637,
+                                    lng: 174.801782
+                                }}
+                                centerAroundCurrentLocation={false}
+                            >
+                            {/* Markers here */}
+                            </Map>
+                        </div>
                 </div>
             </section>
         )
@@ -22,4 +38,6 @@ class BrowseMap extends Component {
     }
 }
 
-export default BrowseMap
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyD5lA7MpAm577yhx-Y8xh22w69mA3qmVAY'
+})(BrowseMap)
