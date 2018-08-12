c169aab9097210e3a00b26e8e12705c433e6e2fb
talor-hammond <talorjhammond@gmail.com>
2018-08-12 22:56:20 +1200

    init marker events

--- client/components/BrowseMap.jsx
+++ client/components/BrowseMap.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react'
 
 // Components:
 // import Map from './Map'
@@ -13,11 +13,13 @@ class BrowseMap extends Component {
         this.state = {
             initialRegion: {}, // parsed and set w browser's geolocation
             posts: [
-            { title: 'EDA STUDENT GRADUATION', topic: 'Celebration!', description: 'Bring your friends and family for EDA students presentation and appreciation. There will be nibbles and beer so come along!! 5:30pm 26th July.', lat: -41.2969355, long: 174.7734782, user_id: 1 },
-            { title: 'Need Jumper Leads', topic: 'Car Problems', description: 'Got a flat battery, if anyone has any jumper leads and would like to help, let me know.', lat: -41.2963787, long: 174.7688924, user_id: 2 },
-            { title: 'BBQ & Chill', topic: 'Event', description: 'MEAT, MEAT, MEAT!!! Come along for the hottest get together in town. Starts at 7pm', lat: -41.2962902, long: 174.772681, user_id: 3 },
-            { title: 'Yoga Session', topic: 'Event', description: 'Spiritual yoga session at Waitangi Park. Beginners welcome!. 6am sharp.', lat: -41.2919435, long: 174.7825271, user_id: 4 },
-            { title: 'Casual Conversations', topic: 'Socialise', description: 'Free your mind and talk with open minded people. @3pm', lat: -41.2948087, long: 174.7747454, user_id: 5 }]
+                    { title: 'EDA STUDENT GRADUATION', topic: 'Celebration!', description: 'Bring your friends and family for EDA students presentation and appreciation. There will be nibbles and beer so come along!! 5:30pm 26th July.', lat: -41.2969355, long: 174.7734782, user_id: 1 },
+                    { title: 'Need Jumper Leads', topic: 'Car Problems', description: 'Got a flat battery, if anyone has any jumper leads and would like to help, let me know.', lat: -41.2963787, long: 174.7688924, user_id: 2 },
+                    { title: 'BBQ & Chill', topic: 'Event', description: 'MEAT, MEAT, MEAT!!! Come along for the hottest get together in town. Starts at 7pm', lat: -41.2962902, long: 174.772681, user_id: 3 },
+                    { title: 'Yoga Session', topic: 'Event', description: 'Spiritual yoga session at Waitangi Park. Beginners welcome!. 6am sharp.', lat: -41.2919435, long: 174.7825271, user_id: 4 },
+                    { title: 'Casual Conversations', topic: 'Socialise', description: 'Free your mind and talk with open minded people. @3pm', lat: -41.2948087, long: 174.7747454, user_id: 5 }
+                ],
+            showingInfoWindow: false
         }
     }
 
@@ -29,15 +31,13 @@ class BrowseMap extends Component {
 
     // Marker events:
     onMouseoverMarker(props, marker, e) {
-        console.log('Props: ', props)
-        console.log('Marker: ', marker)
-        console.log('e: ', e)
+        
     }
 
-    onClickMarker(props, marker, e) {
-        console.log('Props: ', props)
-        console.log('Marker: ', marker)
-        console.log('e: ', e)
+    onMarkerClick(props, marker, e) {
+        this.setState({
+        showingInfoWindow: true
+        })
     }
 
     render() {
@@ -71,11 +71,18 @@ class BrowseMap extends Component {
                                             lng: post.long
                                         }}
                                         onMouseover={this.onMouseoverMarker}
-                                        onClick={this.onClickMarker}
+                                        onClick={this.onMarkerClick}
                                     />
                                 )
                             })
                         }
+                        <InfoWindow
+                            marker={posts[0]}
+                            visible={this.state.showingInfoWindow}>
+                                <div>
+                                    <h1>Wellington</h1>
+                                </div>
+                        </InfoWindow>
                     </Map>
                 </div>
             </div>
