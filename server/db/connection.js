29b00d474070315ce51e548a4a0d534c273ff946
talor-hammond <talorjhammond@gmail.com>
2018-08-14 19:15:39 +1200

    changed connection config, development -> production

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,5 @@
 const knex = require('knex')
-const config = require('../../knexfile')[process.env.NODE_ENV || 'development']
+const config = require('../../knexfile')[process.env.NODE_ENV || 'production']
 const connection = knex(config)
 
 module.exports = connection
