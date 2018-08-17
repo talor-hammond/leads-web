3e32a509299461cf69624b5be5992591c9e760c1
talor-hammond <talorjhammond@gmail.com>
2018-08-17 16:44:28 +1200

    getGeneralPosts returns the correct array of data

--- server/db/general_posts.js
+++ server/db/general_posts.js
@@ -5,10 +5,5 @@ function getGeneralPosts(testDb) { // comes with relevant user data
 
     return db('general_posts')
         .join('users', 'general_posts.user_id', 'users.id')
-        .select('general_posts.user_id as post.id, users.id as user_id, user_name, email, avatar, category, title, description, address, lat, lng, published')
-        .then(results => {
-            console.log(results)
-        })
+        .select('general_posts.id as post_id', 'category', 'title', 'description', 'address', 'lat', 'lng', 'published', 'user_id', 'user_name', 'email', 'avatar')
 }
-
-getGeneralPosts()
