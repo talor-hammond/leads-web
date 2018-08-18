becc0bf27de5d3f754e3a0963f28ac3f39eb9d71
talor-hammond <talorjhammond@gmail.com>
2018-08-18 13:56:35 +1200

    init general reducer

---
+++ client/reducers/general_posts.js
@@ -0,0 +1,12 @@
+import {
+    GET_POSTS,
+    ADD_POST,
+    GET_POST_BY_POST_ID,
+    GET_POSTS_BY_USER_ID
+} from '../actions/general_posts'
+
+export function general(state = [], action) {
+    switch (action.type) {
+
+    }
+}
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux'
 
 import auth from './auth'
+import general from './general_posts'
 
 export default combineReducers({
-  auth
+  auth,
+  general
 })
