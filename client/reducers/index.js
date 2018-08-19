07c147a5709273d6e0a319c0224488be05512186
talor-hammond <talorjhammond@gmail.com>
2018-08-19 16:48:11 +1200

    combined comments reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux'
 
 import auth from './auth'
 import general_posts from './general_posts'
+import comments from './comments'
 
 export default combineReducers({
   auth,
-  general_posts
+  general_posts,
+  comments
 })
