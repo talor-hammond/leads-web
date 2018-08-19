43ef0113ed00dc8856c8cd098b05b2d920407a35
talor-hammond <talorjhammond@gmail.com>
2018-08-19 17:12:47 +1200

    complete initial comments reducer

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,11 @@
+import {
+    GET_COMMENTS
+} from '../actions/comments'
+
 export default function comments(state = [], action) {
     switch (action.type) {
+        case GET_COMMENTS:
+            return [...action.comments]
         default:
             return state
     }
