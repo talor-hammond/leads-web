0def4a65ed5bfaea465651cbe26212ddb45f11d7
talor-hammond <talorjhammond@gmail.com>
2018-08-22 13:31:19 +1200

    init general_posts reducer test suite

---
+++ test/client/reducers/general_posts.test.js
@@ -0,0 +1,21 @@
+import general_posts from '../../../client/reducers/general_posts'
+
+import {
+    GET_POSTS,
+    GET_POST_BY_POST_ID,
+    GET_POSTS_BY_USER_ID,
+    ADD_POST
+} from '../../../client/actions/general_posts'
+
+import {
+    fakePosts,
+    fakePost
+} from '../mocks/mocks'
+
+describe('general_posts reducer', () => {
+    it('returns default state')
+    it('handles GET_POSTS')
+    it('handles GET_POST_BY_POST_ID')
+    it('handles GET_POSTS_BY_USER_ID')
+    it('handles ADD_POST')
+})
