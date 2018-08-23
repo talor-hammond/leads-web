20aff7b85e4e3e2d60d48b982824423d0f79055d
talor-hammond <talorjhammond@gmail.com>
2018-08-23 15:51:05 +1200

    init Create.jsx

---
+++ client/components/Create.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+// components
+import CreateForm from './CreateForm'
+
+class Create extends Component {
+    render() {
+        <CreateForm />
+    }
+}
+
+export default Create
