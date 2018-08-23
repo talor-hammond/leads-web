5c03a294b0d55f2f4facbdab1bfe6fc234703d97
talor-hammond <talorjhammond@gmail.com>
2018-08-23 19:07:05 +1200

    scrolls to form when category selected

--- client/components/Create.jsx
+++ client/components/Create.jsx
@@ -14,10 +14,21 @@ class Create extends Component {
         }
 
         this.select = this.select.bind(this)
+        this.handleScrollToElement = this.handleScrollToElement.bind(this)
     }
 
     select() {
-        this.setState({ categorySelected: true })
+        this.setState({ categorySelected: true }, () => {
+            this.handleScrollToElement()
+        })
+    }
+
+    handleScrollToElement() {
+        scrollToComponent(this.refs.form, {
+            offset: 0,
+            align: 'top',
+            duration: 800
+        })
     }
 
     render() {
@@ -30,26 +41,26 @@ class Create extends Component {
                     <div className="container">
                         <h1 className="title is-1">Choose a category</h1>
                         <div className="columns categories">
-                            <div className="column category" onClick={() => this.select()}>
-                                    <h1 className="title is-3">General</h1>
-                                    <h2 className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur repellat sint, accusamus perspiciatis fugit, doloribus alias cum neque soluta sed sunt nostrum eaque unde quisquam ratione ad quam voluptatem!</h2>
+                            <div className="column category general" onClick={() => this.select()}>
+                                <h1 className="title is-3">General</h1>
+                                <h2 className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consequatur repellat sint, accusamus perspiciatis fugit, doloribus alias cum neque soluta sed sunt nostrum eaque unde quisquam ratione ad quam voluptatem!</h2>
                             </div>
-                            <div className="column category" onClick={() => this.select()}>
-                                    <h1 className="title is-3">Services</h1>
-                                    <h2 className="subtitle">Have some skills to offer to the community? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis voluptatum maiores sequi aliquid provident? Numquam quisquam ullam fuga laborum?</h2>
+                            <div className="column category services" onClick={() => this.select()}>
+                                <h1 className="title is-3">Services</h1>
+                                <h2 className="subtitle">Have some skills to offer to the community? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum veritatis voluptatum maiores sequi aliquid provident? Numquam quisquam ullam fuga laborum?</h2>
                             </div>
-                            <div className="column category" onClick={() => this.select()}>
-                                    <h1 className="title is-3">Jobs</h1>
-                                    <h2 className="subtitle">Think someone in the community might be able to help you with something? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, nam?</h2>
+                            <div className="column category jobs" onClick={() => this.select()}>
+                                <h1 className="title is-3">Jobs</h1>
+                                <h2 className="subtitle">Think someone in the community might be able to help you with something? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, nam?</h2>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                {categorySelected && (
-                    <CreateForm />
-                )}
-                
+                    {categorySelected && (
+                        <CreateForm ref="form" />
+                    )}
+
             </section>
         )
     }
