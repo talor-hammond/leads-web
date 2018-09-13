cfdd1baea0d85e3ef08b807348bff6772aa9bc8b
talor-hammond <talorjhammond@gmail.com>
2018-09-13 20:01:52 +1200

    rm logs

--- client/utils/localstorage.js
+++ client/utils/localstorage.js
@@ -5,7 +5,7 @@ export function get (key) {
 }
 
 export function set (key, value) {
-  console.log('setting ' + key + ' with value: ' + value) // NOTE: gets set correctly the first time, then 'refreshes' -- set as null????
+  // console.log('setting ' + key + ' with value: ' + value) // NOTE: gets set correctly the first time, then 'refreshes' -- set as null????
 
   if(value === null) {
     localStorage.removeItem(key)
